3627b957b9826a1204bbb5469447a9eba3622644
Brandon Might <might.brandon@gmail.com>
2020-03-12 13:18:38 -0400

    Application build setup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+// rollup.config.js
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    // pass custom options to the resolve plugin
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    typescript({ module: 'CommonJS' }),
+    commonjs({extensions: ['.js', '.ts']}),
+    postcss({
+      extract: `dist/${pkg.name}.css`
+    })
+  ],
+  // indicate which modules should be treated as external
+  external: ['react', 'react-dom', 'formik', 'reactstrap', 'bootstrap']
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table: React.FC = () => {
+  return (
+    <div>Placeholder for project setup</div>
+  )
+}
+
+export default Table
